7168660f4743cfbd4d2358a2a7d10f82da99dbd5
Andres Cruz <e5ce6c2@gmail.com>
2023-05-18 22:13:47 -0700

    Created baremetal program that will run on Arduino Uno

---
+++ Firmware/uno-firmware/uno-firmware.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+#define I2C_ADDRESS         0x68
+#define I2C_SLAVE_ADDRESS   b1101000
+
+int main(void)
+{
+    // Set I2C frequency
+    // Enable I2C
+}
+
+void startI2CTransmission()
+{
+
+}
+
+
+void sendI2CData(uint8_t data)
+{
+
+}
+
+uint8_t readI2CData()
+{
+    
+}
+
+void stopI2CTransmission()
+{
+
+}
